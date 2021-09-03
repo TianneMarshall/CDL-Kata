63d6b4d8a28f4ddeb66135d53cf1814742bfa912
TianneMarshall <tiannemarshall@outlook.com>
2021-09-03 05:14:22 +0100

    Added new class objects to represent products and set their prices in Main class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Item A = new Item();
+		Item B = new Item();
+		Item C = new Item();
+		Item D = new Item();
+		
+		A.setPrice(50);
+		
+		B.setPrice(30);
+		
+		C.setPrice(20);
+		
+		D.setPrice(15);
 	}
 
 }
